c6caf7bdf3791e6dd12e0d6447bfffc0756e2a57
unknown <kevin.sm89@gmail.com>
2025-05-14 20:26:34 +0200

    first commit de la nueva rama

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from resources import api_blueprint
 from config import Config
 from database import db
 from flask_cors import CORS
+import time
+from sqlalchemy.exc import OperationalError
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -17,5 +21,14 @@ app.register_blueprint(api_blueprint)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        for _ in range(10):
+            try:
+                db.create_all()
+                break
+            except OperationalError:
+                print("Database not ready yet. Retrying in 3 seconds...")
+                time.sleep(3)
+
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+    
--- config.py
+++ config.py
@@ -1,6 +1,24 @@
+# import os
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'myjwtsecret')
+
+# import os
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://user:password@db:5432/mydatabase')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'myjwtsecret')
+
+
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+    'DATABASE_URI',
+    'mysql+pymysql://root:rootpassword@db:3306/midb'
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'myjwtsecret')
